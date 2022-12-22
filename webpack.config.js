631ca28d66e644f61d19bba0a7ddddb90a2cc06a
ambi hidalgo <ambio.pk@gmail.com>
2022-12-21 18:34:14 -0800

    Add HtmlWebpackPluginwith an entry point, add index.html and index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 	
 module.exports = {
   	entry: './src/index.js',
   	output: {
     	filename: 'bundle.js',
    	 path: path.resolve(__dirname, 'dist')
- 	 }	
+ 	 },
+   plugins:[
+   new HtmlWebpackPlugin({
+    title: 'galactic-age-calculator',
+    template: './src/index.html',
+    inject: 'body'
+   
+   })
+  ],	
   };
