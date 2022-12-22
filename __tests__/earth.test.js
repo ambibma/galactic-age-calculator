f93727eb39cdb014ed4cfce9271c715cc2d4abae
ambi hidalgo <ambio.pk@gmail.com>
2022-12-21 21:12:48 -0800

    Test Failed expectedly in calculating years between next earth birthday

--- __tests__/earth.test.js
+++ __tests__/earth.test.js
@@ -1,5 +1,5 @@
 
-import UserAge from "../src/earth";
+import {UserAge, FutureAge } from "../src/earth";
 
 describe('UserAge', () => {
   let userAge;
@@ -15,4 +15,11 @@ describe('UserAge', () => {
   test('it should return the inputted age', ()=>{
     expect(userAge.displayEarthAge(userAge.years)).toEqual(28);
   });
+
 });
+describe('FutureAge', () =>{  
+  test('it should return the future inputted age in earth years', ()=>{
+    let futureAge = new FutureAge(50);
+    expect(futureAge.years).toEqual(50);
+  });
+})
--- src/earth.js
+++ src/earth.js
@@ -1,10 +1,16 @@
-
-export default class UserAge {
+export {UserAge, FutureAge}
+class UserAge {
   constructor(age){
   this.years = age;
   this.days = age * 365;
   }
   displayEarthAge() {
     return this.years;
+  }
+  
+}
+class FutureAge {
+  constructor(){
+    
   }
 }
