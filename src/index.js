19b58965ea715c3f8dea6cf139e6df5006a70b97
ambi hidalgo <ambio.pk@gmail.com>
2022-12-23 21:47:54 -0800

    Add HandleFormSubmission that will print ages from past,present future birthdays

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ function handleFormSubmit(event){
   let age = document.getElementById('age').value;
   let futureAge = document.getElementById('futureAge').value;
   let pastAge = document.getElementById('pastAge').value;
-  let results = document.querySelector('div.box');
-  results.classList.remove('hidden');
+  let resultsPast = document.getElementById('pastResults');
+  let resultsPresent = document.getElementById('presentResults');
+  let resultsFuture = document.getElementById('futureResults');
+  resultsPast.classList.remove('hidden');
+  resultsPresent.classList.remove('hidden');
+  resultsFuture.classList.remove('hidden');
   const earthPast = document.getElementById('earthPast');
   const mercuryPast = document.getElementById('mercuryPast');
   const venusPast = document.getElementById('venusPast');
@@ -25,14 +29,24 @@ function handleFormSubmit(event){
   const venusFuture = document.getElementById('venusFuture');
   const marsFuture = document.getElementById('marsFuture');
   const jupiterFuture = document.getElementById('jupiterFuture');
-  earthPast.innerText = galacticAge.pastEarthBirthday()
-
-
   galacticAge.earthYears = age;
   galacticAge.pastEarthAge = pastAge;
   galacticAge.futureEarthAge = futureAge;
-  
-
-
+  earthPast.innerText = galacticAge.pastEarthBirthday(pastAge);
+  mercuryPast.innerText = galacticAge.pastMercuryBirthday();
+  venusPast.innerText = galacticAge.pastVenusBirthday();
+  marsPast.innerText = galacticAge.pastMarsBirthday();
+  jupiterPast.innerText = galacticAge.pastJupiterBirthday();
+  earthPresent.innerText = galacticAge.displayEarthAge();
+  mercuryPresent.innerText = galacticAge.toMercuryAge();
+  venusPresent.innerText = galacticAge.toVenusAge();
+  marsPresent.innerText = galacticAge.toMarsYears();
+  jupiterPresent.innerText = galacticAge.toJupiterYears();
+  earthFuture.innerText = galacticAge.nextEarthBirthday();
+  mercuryFuture.innerText = galacticAge.nextMercuryBirthday();
+  venusFuture.innerText = galacticAge.nextVenusBirthday();
+  marsFuture.innerText = galacticAge.nextMarsBirthday();
+  jupiterFuture.innerText = galacticAge.nextJupiterBirthday();
+  // document.getElementById("ageForm").reset();
 }
 document.getElementById("ageForm").addEventListener("submit", handleFormSubmit);
